82203635c5e35432de54ea9ab7a8b04c90857a52
Devon Ray <devon@devonray.net>
2020-02-13 19:45:28 +0000

    Added stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, commands, window, workspace } from "vscode";
+import { ExtensionContext, commands, window} from "vscode";
 import { LoginForm } from "./components/login_form";
 import { AddSnippForm } from "./components/add_snipp";
-import { SnippProvider, Dependency } from "./providers/snippProvider";
+import { SnippExplorer } from "./providers/snippProvider";
 
 
 export function activate(context: ExtensionContext) {
 
-  const snippProvider = new SnippProvider(workspace.rootPath!, context);
-  window.registerTreeDataProvider('allSnipps', snippProvider);
-  commands.registerCommand('allSnipps.refreshEntry', () => snippProvider.refresh());
-	commands.registerCommand('allSnipps.addEntry', () => window.showInformationMessage(`Successfully called add entry.`));
-	commands.registerCommand('allSnipps.editEntry', (node: Dependency) => window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	commands.registerCommand('allSnipps.deleteEntry', (node: Dependency) => window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+  new SnippExplorer(context);
+	// commands.registerCommand('allSnipps.addEntry', () => window.showInformationMessage(`Successfully called add entry.`));
+	// commands.registerCommand('allSnipps.editEntry', (node: Dependency) => window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+	// commands.registerCommand('allSnipps.deleteEntry', (node: Dependency) => window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
   context.subscriptions.push(
     commands.registerCommand("extension.createSnipp", async () => {
       AddSnippForm(context);
--- src/providers/SnippProvider.ts
+++ src/providers/SnippProvider.ts
@@ -1,76 +1,99 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
+import { basename, dirname } from "path";
 
-export class SnippProvider implements vscode.TreeDataProvider<Dependency> {
-  private _onDidChangeTreeData: vscode.EventEmitter<
-    Dependency | undefined
-  > = new vscode.EventEmitter<Dependency | undefined>();
-  readonly onDidChangeTreeData: vscode.Event<Dependency | undefined> = this
-    ._onDidChangeTreeData.event;
+export interface Snipp {
+  name: string;
+  tags: string[];
+  content: string | null;
+  contentType: string | null;
+}
+
+export class SnippModel {
+  private nodes: Map<string, Snipp> = new Map<string, Snipp>();
 
   constructor(
-    private workspaceRoot: string,
+    readonly view: string,
     private context: vscode.ExtensionContext
-  ) {
+  ) {}
+
+  public get roots(): Thenable<Snipp[]> {
+    return Promise.resolve(this.context?.globalState?.get("snipps", []));
   }
 
-  refresh(): void {
-    this._onDidChangeTreeData.fire();
+  public getChildren(node: Snipp): Thenable<Snipp[]> {
+    return Promise.resolve([]);
+    // return Promise.resolve(this.context?.globalState?.get("snipps", []));
   }
 
-  getTreeItem(element: Dependency): vscode.TreeItem {
-    return element;
-  }
-
-  getChildren(element?: Dependency): Thenable<Dependency[]> {
-    interface Snipp {
-      name: string;
-      tags: string[];
-      content: string | null;
-      contentType: string | null;
-    }
-    return Promise.resolve(this.context?.globalState?.get("snipps", []).map((snipp: Snipp) => (new Dependency(snipp.name, "1.3", 1))));
+  public getContent(resource: vscode.Uri): Thenable<string> {
+    return Promise.resolve("");
   }
 }
 
-export class Dependency extends vscode.TreeItem {
+export class SnippProvider
+  implements
+    vscode.TreeDataProvider<Snipp>,
+    vscode.TextDocumentContentProvider {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    any
+  > = new vscode.EventEmitter<any>();
+  readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData
+    .event;
+
   constructor(
-    public readonly label: string,
-    private version: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-    public readonly command?: vscode.Command
-  ) {
-    super(label, collapsibleState);
+    private readonly model: SnippModel,
+    private context: vscode.ExtensionContext
+  ) {}
+
+  public refresh(): any {
+    this._onDidChangeTreeData.fire();
   }
 
-  get tooltip(): string {
-    return `${this.label}-${this.version}`;
+  public getTreeItem(element: Snipp): vscode.TreeItem {
+    return {
+      label: element.name
+    };
   }
 
-  get description(): string {
-    return this.version;
+  public getChildren(element?: Snipp): Snipp[] | Thenable<Snipp[]> {
+    return element ? this.model.getChildren(element) : this.model.roots;
   }
 
-  // iconPath = {
-  //   light: path.join(
-  //     __filename,
-  //     "..",
-  //     "..",
-  //     "..",
-  //     "resources",
-  //     "dependency.svg"
-  //   ),
-  //   dark: path.join(
-  //     __filename,
-  //     "..",
-  //     "..",
+  // public getParent(element: Snipp): Snipp {
 
-  //     "..",
-  //     "resources",
-  //     "dependency.svg"
-  //   )
-  // };
+  //   const parent = element.resource.with({
+  //     path: dirname(element.resource.path)
+  //   });
+  //   return parent.path !== "//"
+  //     ? { resource: parent, isDirectory: true }
+  //     : null;
+  // }
 
-  // contextValue = "createSnipp";
+  public provideTextDocumentContent(
+    uri: vscode.Uri,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<string> {
+    return this.model.getContent(uri).then(content => content);
+  }
+}
+
+export class SnippExplorer {
+  private snippViewer: vscode.TreeView<Snipp>;
+
+  constructor(context: vscode.ExtensionContext) {
+    const snippModel = new SnippModel("recent", context);
+
+    const snippDataProvider = new SnippProvider(snippModel, context);
+
+    this.snippViewer = vscode.window.createTreeView("allSnipps", {
+      treeDataProvider: snippDataProvider
+    });
+
+    vscode.commands.registerCommand("allSnipps.refreshEntry", () =>
+      snippDataProvider.refresh()
+    );
+    vscode.commands.registerCommand("allSnipps.addEntry", () =>
+      vscode.window.showInformationMessage(`Successfully called add entry.`)
+    );
+  }
 }
